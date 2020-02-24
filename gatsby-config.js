5b283c028bb36cc0e5b1010b92a0dac2fad9a325
kovsky <johan@developpeur-web.tech>
2020-02-24 07:07:52 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,13 @@
+require('dotenv').config({
+  path: `.env`,
+});
+
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Spinat Default Starter`,
+    description: `Best web agency EU`,
+    author: `@kovskyD`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +16,15 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: 'spinat',
+        accessToken: `${process.env.API_KEY}`,
+        // Get the correct URLs in blog posts
+        linkResolver: () => post => `/${post.uid}`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,67 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Img from 'gatsby-image'
+import BackgroundImage from 'gatsby-background-image'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <BackgroundImage
+      Tag="section"
+      fluid={
+        data.prismicHomepage.data.body[0].primary.hero_image1.localFile
+          .childImageSharp.fluid
+      }
+      backgroundColor="#040e18"
+    >
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.prismicHomepage.data.body[0].primary.title_hero.html,
+        }}
+      />
+      <p>{data.prismicHomepage.data.body[0].primary.button_text}</p>
+      <p>{data.prismicHomepage.data.body[0].primary.button_link.slug}</p>
+      <h2>gatsby-background-image</h2>
+    </BackgroundImage>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    prismicHomepage {
+      data {
+        body {
+          primary {
+            title_hero {
+              html
+              text
+            }
+            button_link {
+              url
+              target
+            }
+            button_text
+            hero_image1 {
+              localFile {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
