ec9aebb420123d698ca4f5289a5b382ec9a711d8
kovsky <johan@developpeur-web.tech>
2020-02-26 22:14:34 +0100

    detail service ok

--- src/templates/Service.jsx
+++ src/templates/Service.jsx
@@ -1,10 +1,44 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 
-const Service = () => (
+const Service = ({ data }) => (
   <div>
-    <Layout>Service</Layout>
+    {console.log(data)}
+    <Layout>
+      Big service inst it
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.prismicServices.data.title.html,
+        }}
+      />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.prismicServices.data.page_content.html,
+        }}
+      />
+    </Layout>
   </div>
 );
+Service.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default Service;
+
+export const pageQuery = graphql`
+  query ServiceQuery {
+    prismicServices(uid: { eq: "product-design" }) {
+      uid
+      data {
+        title {
+          html
+        }
+        page_content {
+          html
+        }
+      }
+    }
+  }
+`;
