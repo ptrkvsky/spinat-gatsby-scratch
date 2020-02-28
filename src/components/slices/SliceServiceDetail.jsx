14b3568a463125497290a1cfc99117449e81dcb3
kovsky <johan@developpeur-web.tech>
2020-02-28 07:19:25 +0100

    reduce font awesome import

--- src/components/slices/SliceServiceDetail.jsx
+++ src/components/slices/SliceServiceDetail.jsx
@@ -2,16 +2,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'gatsby';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, fas } from '@fortawesome/free-solid-svg-icons';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
-library.add(fas, fab);
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const SliceServiceDetail = ({ item }) => (
   <div>
-    <FontAwesomeIcon icon={['fab', 'apple']} />
     Favorite beverage: <FontAwesomeIcon icon={faCoffee} />
     <Link to={`service/${item.link_service.slug}`}>service 1</Link>
   </div>
